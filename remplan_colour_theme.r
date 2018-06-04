a69ea107514b99693a3ee0d8b665de8c2be74134
Alex Kelly <lexkel@hotmail.com>
2018-06-04 13:00:44 +1000

    Remplan colour palette created.

---
+++ remplan_colour_theme.r
@@ -0,0 +1,81 @@
+
+  #====================================================================================================
+  #
+  # REMPLAN colour palette
+  #
+  # Source: https://drsimonj.svbtle.com/creating-corporate-colour-palettes-for-ggplot2
+  #====================================================================================================
+  
+  library(tidyverse)
+  
+  # Remplan colour theme
+  remplan_colours <- c(remplan     = "#D71335",
+                       community   = "#00ACE5", 
+                       economy     = "#F17C33",
+                       health      = "#A3258D",
+                       investment  = "#53308C",
+                       business    = "#00619C",
+                       environment = "#44B04E",
+                       forecast    = "#006D5D",
+                       mapbuilder  = "#E90080",
+                       consulting  = "#FEC929",
+                       black       = "#000000",
+                       grey        = "#808285")
+  
+  # Functions to extract colours and create palettes
+  get_remplan_cols <- function(...) {
+    cols <- c(...)
+    if (is.null(cols))
+      return (remplan_colours)
+    remplan_colours[cols]
+  }
+  
+  # Remplan colour palettes
+  remplan_palettes <- list(warm = get_remplan_cols("remplan", "economy", "consulting"),
+                           cool = get_remplan_cols("community", "environment"),
+                           fcst2 = get_remplan_cols("forecast", "environment"),
+                           main2 = get_remplan_cols("remplan", "economy"),
+                           all8 = get_remplan_cols("remplan", "economy", "consulting", "environment", "forecast", "community", "business", "investment"))
+  
+  # Functions to extract colours
+  get_remplan_palette <- function(palette = "warm", reverse = FALSE, ...) {
+    pal <- remplan_palettes[[palette]]
+    if (reverse) pal <- rev(pal)
+    colorRampPalette(pal, ...)
+  }
+  
+  scale_color_remplan <- function(palette = "warm", discrete = TRUE, reverse = FALSE, ...) {
+    pal <- get_remplan_palette(palette = palette, reverse = reverse)
+    if (discrete) {
+      discrete_scale("colour", paste0("remplan_", palette), palette = pal, ...)
+    } else {
+      scale_color_gradientn(colours = pal(256), ...)
+    }
+  }
+  
+  scale_fill_remplan <- function(palette = "main", discrete = TRUE, reverse = FALSE, ...) {
+    pal <- get_remplan_palette(palette = palette, reverse = reverse)
+    if (discrete) {
+      discrete_scale("fill", paste0("remplan_", palette), palette = pal, ...)
+    } else {
+      scale_fill_gradientn(colours = pal(256), ...)
+    }
+  }
+  
+  # Example of ggplot with remplan colours
+  ggplot(iris, aes(Sepal.Width, Sepal.Length, color = Species)) +
+          geom_point(size = 4) +
+          scale_color_remplan()
+  
+  # Example of ggplot with remplan colours
+  theme_set(theme_minimal())
+  
+  ggplot(iris, aes(Sepal.Width, Sepal.Length, color = Species)) +
+    geom_point(size = 4) +
+    scale_color_remplan()
+  
+  ggplot(mpg, aes(manufacturer, fill = manufacturer)) +
+    geom_bar() +
+    theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+    scale_fill_remplan(palette = "all8", guide = "none")
+  
